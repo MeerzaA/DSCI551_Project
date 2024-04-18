78954cb467b1b02de33c39a46e194fd31bfed0da
perkinsm27 <mp43756@usc.edu>
2024-04-17 20:10:09 -0700

    remove sidebar component

--- Cafe_Yelp_WebApp/hosting/src/app/app.routes.ts
+++ Cafe_Yelp_WebApp/hosting/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+//import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ContentComponent } from './content/content.component';
 
